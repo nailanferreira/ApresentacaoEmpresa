dbf9b75a1d892740dd4d522d23131ffb14a703cb
55119 <55119@DESKTOP-FMLFQCR>
2022-02-18 17:20:37 -0300

    exemplo commitando uma nova classe

---
+++ primeiro_projeto/src/primeiro_projeto/Exemplo.java
@@ -0,0 +1,19 @@
+package primeiro_projeto;
+
+public class Exemplo {
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	String nome;
+	int idade;
+
+}
